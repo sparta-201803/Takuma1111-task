b37dc2c0ba96b695ca3f4cedcc3c821d8676c073
村上拓麻 <erekguitar0120@gmail.com>
2018-03-13 14:57:49 +0900

    second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(
+  title: "タスク1",
+  body: "タスク1の内容"
+)
+
+Task.create(
+  title: "タスク2",
+  body: "タスク2の内容"
+)
+
+Task.create(
+  title: "タスク3",
+  body: "タスク3の内容"
+)
